6f2b4c658049d801b7dabce34765172762cf14a3
JULIE <jiamiaoc12@gmail.com>
2023-02-07 16:45:09 -0500

    udate

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,16 @@
 exports.factorial = function(num) {
+    // need to multiple all the number from 1 to num 
+    let arrayOfNum = [];
+
+    for(let i = 1; i <= num; i++){
+        arrayOfNum.push(i);
+    }
+    // output is the product of all that number up to num
+    let newArrOfNum = 1
+    for(let i = 0; i < arrayOfNum.length; i++){
+        newArrOfNum = newArrOfNum * arrayOfNum[i] 
+    }
+return newArrOfNum
 };
+
+
